0e78d12ad7e455e3343e40298f4daa63f3abd2bc
Joachim Metz <joachim.metz@gmail.com>
2016-08-29 07:36:35 +0200

    Worked on tests.

---
+++ tests/fusn_test_error.c
@@ -0,0 +1,149 @@
+/*
+ * Library error functions test program
+ *
+ * Copyright (C) 2011-2016, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+
+#if defined( HAVE_STDLIB_H ) || defined( WINAPI )
+#include <stdlib.h>
+#endif
+
+#include "fusn_test_libcerror.h"
+#include "fusn_test_libfusn.h"
+#include "fusn_test_macros.h"
+#include "fusn_test_unused.h"
+
+/* Tests the libfusn_error_free function
+ * Returns 1 if successful or 0 if not
+ */
+int fusn_test_error_free(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libfusn_error_free(
+	 NULL );
+
+	return( 1 );
+}
+
+/* Tests the libfusn_error_fprint function
+ * Returns 1 if successful or 0 if not
+ */
+int fusn_test_error_fprint(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libfusn_error_fprint(
+	 NULL,
+	 NULL );
+
+	return( 1 );
+}
+
+/* Tests the libfusn_error_sprint function
+ * Returns 1 if successful or 0 if not
+ */
+int fusn_test_error_sprint(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libfusn_error_sprint(
+	 NULL,
+	 NULL,
+	 0 );
+
+	return( 1 );
+}
+
+/* Tests the libfusn_error_backtrace_fprint function
+ * Returns 1 if successful or 0 if not
+ */
+int fusn_test_error_backtrace_fprint(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libfusn_error_backtrace_fprint(
+	 NULL,
+	 NULL );
+
+	return( 1 );
+}
+
+/* Tests the libfusn_error_backtrace_sprint function
+ * Returns 1 if successful or 0 if not
+ */
+int fusn_test_error_backtrace_sprint(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libfusn_error_backtrace_sprint(
+	 NULL,
+	 NULL,
+	 0 );
+
+	return( 1 );
+}
+
+/* The main program
+ */
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+int wmain(
+     int argc FUSN_TEST_ATTRIBUTE_UNUSED,
+     wchar_t * const argv[] FUSN_TEST_ATTRIBUTE_UNUSED )
+#else
+int main(
+     int argc FUSN_TEST_ATTRIBUTE_UNUSED,
+     char * const argv[] FUSN_TEST_ATTRIBUTE_UNUSED )
+#endif
+{
+	FUSN_TEST_UNREFERENCED_PARAMETER( argc )
+	FUSN_TEST_UNREFERENCED_PARAMETER( argv )
+
+	FUSN_TEST_RUN(
+	 "libfusn_error_free",
+	 fusn_test_error_free() )
+
+	FUSN_TEST_RUN(
+	 "libfusn_error_fprint",
+	 fusn_test_error_fprint() )
+
+	FUSN_TEST_RUN(
+	 "libfusn_error_sprint",
+	 fusn_test_error_sprint() )
+
+	FUSN_TEST_RUN(
+	 "libfusn_error_backtrace_fprint",
+	 fusn_test_error_backtrace_fprint() )
+
+	FUSN_TEST_RUN(
+	 "libfusn_error_backtrace_sprint",
+	 fusn_test_error_backtrace_sprint() )
+
+	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
+}
+
--- tests/fusn_test_get_version.c
+++ tests/fusn_test_get_version.c
@@ -20,7 +20,6 @@
  */
 
 #include <common.h>
-#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
@@ -28,6 +27,7 @@
 
 #include "fusn_test_libcstring.h"
 #include "fusn_test_libfusn.h"
+#include "fusn_test_macros.h"
 #include "fusn_test_unused.h"
 
 /* Tests retrieving the library version
@@ -46,11 +46,15 @@ int fusn_test_get_version(
 	          LIBFUSN_VERSION_STRING,
 	          9 );
 
-	if( result != 0 )
-	{
-		return( 0 );
-	}
+	FUSN_TEST_ASSERT_EQUAL(
+	 "result",
+	 result,
+	 0 );
+
 	return( 1 );
+
+on_error:
+	return( 0 );
 }
 
 /* The main program
@@ -68,10 +72,13 @@ int main(
 	FUSN_TEST_UNREFERENCED_PARAMETER( argc )
 	FUSN_TEST_UNREFERENCED_PARAMETER( argv )
 
-	if( fusn_test_get_version() != 1 )
-	{
-		return( EXIT_FAILURE );
-	}
+	FUSN_TEST_RUN(
+	 "libfusn_get_version",
+	 fusn_test_get_version() )
+
 	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
 }
 
