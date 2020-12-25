7debdbe635288936dfe6393d9bb1cc28ff06c2b6
Joachim Metz <joachim.metz@gmail.com>
2020-12-25 07:08:00 +0100

    Applied updates

--- libfusn/libfusn_extern.h
+++ libfusn/libfusn_extern.h
@@ -30,7 +30,7 @@
 
 #include <libfusn/extern.h>
 
-#if defined( __CYGWIN__ )
+#if defined( __CYGWIN__ ) || defined( __MINGW32__ )
 #define LIBFUSN_EXTERN_VARIABLE	extern
 #else
 #define LIBFUSN_EXTERN_VARIABLE	LIBFUSN_EXTERN
