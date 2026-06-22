a1b24270cbb3fb9c32863f1f963a0b6fc8b51513
Joachim Metz <joachim.metz@gmail.com>
2026-06-22 19:25:18 +0200

    Worked on tests and changes for CI tests

--- include/libfusn/error.h
+++ include/libfusn/error.h
@@ -82,7 +82,7 @@ enum LIBFUSN_ARGUMENT_ERROR
 	 */
 	LIBFUSN_ARGUMENT_ERROR_UNSUPPORTED_VALUE	= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBFUSN_ARGUMENT_ERROR_CONFLICTING_VALUE	= 9
 };
@@ -214,7 +214,7 @@ enum LIBFUSN_OUTPUT_ERROR
 {
 	LIBFUSN_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBFUSN_OUTPUT_ERROR_INSUFFICIENT_SPACE		= 1
 };
