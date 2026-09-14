ca4843bfacfb131675926ae0774efebbb8b6ab13
Joachim Metz <joachim.metz@gmail.com>
2026-09-14 12:52:19 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libfusn/.libs/libfusn.1.dylib && test -f ./pyfusn/.libs/pyfusn.so
+if [ -f "${PWD}/libfusn/.libs/libfusn.1.dylib" ] && [ -f ./pyfusn/.libs/pyfusn.so ]
 then
-	install_name_tool -change /usr/local/lib/libfusn.1.dylib ${PWD}/libfusn/.libs/libfusn.1.dylib ./pyfusn/.libs/pyfusn.so
+    install_name_tool -change /usr/local/lib/libfusn.1.dylib "${PWD}/libfusn/.libs/libfusn.1.dylib" ./pyfusn/.libs/pyfusn.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
